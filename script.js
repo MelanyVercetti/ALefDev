724a2388e7a2cb7be114b31c7bd9831516c3d120
Melany <melany777@mail.ru>
2022-02-08 15:55:15 +0600

    block main is ready

--- script.js
+++ script.js
@@ -8,65 +8,67 @@ menuBtn.addEventListener('click', () => {
     headerNav.classList.toggle('active');
 })
 
+// main-image
 
-// // header-scroll 
-// let lastScroll = 0;
-// const defaultOffset = 200;
-// const header = document.querySelector('.header');
+const mainImage = document.querySelector('.main-image_big-item_pic');
+const smallImages = document.querySelectorAll('.main-image_small-items_pic');
 
-// const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-// const containHide = () => header.classList.contains('hide');
+smallImages.forEach(img => {
+    img.addEventListener('click', () => {
+        mainImage.setAttribute('src', img.getAttribute('src'))
+    })
+})
 
-// window.addEventListener('scroll', () => {
-//     if (scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
-//         header.classList.add('hide');
-//     } else if (scrollPosition() < lastScroll && containHide()) {
-//         header.classList.remove('hide');
-//     }
 
-//     lastScroll = scrollPosition();
-// })
+// header-scroll 
+let lastScroll = 0;
+const defaultOffset = 200;
+const header = document.querySelector('.header');
 
-// // change main photo
-// const mainImage = document.querySelector('.item-pic_img');
-// const itemImgs = document.querySelectorAll('.items-img_item');
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains('hide');
 
-// itemImgs.forEach(el => {
-//     el.addEventListener('click', ev => {
-//         mainImage.setAttribute('src', ev.target.getAttribute('src'));
-//     })
-// });
+window.addEventListener('scroll', () => {
+    if (scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
+        header.classList.add('hide');
+    } else if (scrollPosition() < lastScroll && containHide()) {
+        header.classList.remove('hide');
+    }
 
-// // add/remove count
-// const counterAdd = document.querySelector('.counter-add');
-// const counterRem = document.querySelector('.counter-rem');
-// let amount = parseInt(document.querySelector('.counter-amount').innerText);
+    lastScroll = scrollPosition();
+})
 
-// function declOfNum(number, titles) {
-//     const cases = [2, 0, 1, 1, 1, 2];
-//     return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
-// }
 
-// counterAdd.addEventListener('click', () => {
-//     document.querySelector('.counter-amount').innerText = amount += 1;
-// });
+// add/remove count
+const counterAdd = document.querySelector('.counter-add');
+const counterRem = document.querySelector('.counter-rem');
+let amount = parseInt(document.querySelector('.counter-amount').innerText);
 
-// counterRem.addEventListener('click', () => {
-//     if (amount > 0) {
-//         document.querySelector('.counter-amount').innerText = amount -= 1;
-//     }
-// });
+function declOfNum(number, titles) {
+    const cases = [2, 0, 1, 1, 1, 2];
+    return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+}
 
-// document.querySelector('.item-info_selected').addEventListener('click', () => {
-//     document.querySelector('.item-info_selected').classList.toggle('active')
-//     alert('Товар добавлен в избранное');
-// });
+counterAdd.addEventListener('click', () => {
+    document.querySelector('.counter-amount').innerText = amount += 1;
+});
 
-// // add to cart
-// document.querySelector('.add-btn').addEventListener('click', () => {
-//     if (amount > 0) {
-//         alert(`Товар в количестве ${document.querySelector('.counter-amount').innerText} ${declOfNum(amount, ['единицы', 'единиц', 'единиц'])} добавлен в корзину`);
-//     } else {
-//         alert('Товар не выбран!')
-//     }
-// });
+counterRem.addEventListener('click', () => {
+    if (amount > 0) {
+        document.querySelector('.counter-amount').innerText = amount -= 1;
+    }
+});
+
+document.querySelector('.main-information_selected').addEventListener('click', () => {
+    document.querySelector('.main-information_selected').classList.toggle('active');
+    alert('Товар добавлен в избранное');
+});
+
+// add to cart
+document.querySelector('.add-btn').addEventListener('click', () => {
+    if (amount > 0) {
+        alert(`Товар в количестве ${document.querySelector('.counter-amount').innerText} ${declOfNum(amount, ['единицы', 'единиц', 'единиц'])} добавлен в корзину`);
+    } else {
+        alert('Товар не выбран!')
+    }
+});
